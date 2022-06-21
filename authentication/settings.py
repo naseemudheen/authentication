099f5870b8fddec817a6187a9eee52042ef3e7a7
Naseemudheen <naseemudheene@gmail.com>
2022-06-21 12:57:49 +0530

    imported dj_database_url

--- authentication/settings.py
+++ authentication/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'user/static/js', 'serviceworker.js')
